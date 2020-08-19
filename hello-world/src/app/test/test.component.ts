d6d5170bcf5eb5587da8522e7cd215d1ea2e47bd
Dipak Aher <dipakaher@Dipaks-MacBook-Air.local>
2020-08-19 21:00:41 +0530

    inline html and css in component

--- hello-world/src/app/test/test.component.ts
+++ hello-world/src/app/test/test.component.ts
@@ -1,9 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-test',
-  templateUrl: './test.component.html',
-  styleUrls: ['./test.component.css']
+  //selector: 'app-test',
+  //selector: '.app-test',
+  selector: '[app-test]',
+  //templateUrl: './test.component.html',
+  template: `<div>
+                Welcome inline template
+            </div>`,
+  //styleUrls: ['./test.component.css']
+  styles: [`
+          div{
+                color: red;
+          } 
+    `]
 })
 export class TestComponent implements OnInit {
 
