6254b1cc586d884df364c3488127e155cbc95604
Dipak Aher <dipakaher@Dipaks-MacBook-Air.local>
2020-08-23 16:53:51 +0530

    Interpolation in angular

--- binding/src/app/test/test.component.ts
+++ binding/src/app/test/test.component.ts
@@ -2,11 +2,35 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  templateUrl: './test.component.html',
-  styleUrls: ['./test.component.css']
+  //templateUrl: './test.component.html',
+  template:`
+    <h2> Welcome {{name}} </h2>
+    <h2> {{2+2}} </h2>
+
+    <h2> {{"Hello "+name}} </h2>
+
+    <h2> {{"Length: "+name.length}} </h2>
+    <h2> {{"Uppercase: "+name.toUpperCase()}} </h2>
+
+    <h2> {{"Method call: "+helloUser()}} </h2>
+
+    <!--<h2> {{a=2+2}} </h2> Error: Bindings cannot contain assignments -->
+
+    <!--<h2>{{window.location.href}}</h2>-->
+    <h2>{{siteUrl}}</h2>
+    
+  `,
+  //styleUrls: ['./test.component.css']
+  styles:[]
 })
 export class TestComponent implements OnInit {
 
+  public name = "Deepak";
+  public siteUrl = window.location.href;
+
+  helloUser(){
+    return "Welcome in method..!!"
+  }
   constructor() { }
 
   ngOnInit(): void {
