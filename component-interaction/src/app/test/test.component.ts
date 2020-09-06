828752befe3fa6b06910477970bbd3f097b1a12f
Dipak Aher <dipakaher@Dipaks-MacBook-Air.local>
2020-09-06 13:37:49 +0530

    Sending data from child to parent component

--- component-interaction/src/app/test/test.component.ts
+++ component-interaction/src/app/test/test.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-test',
@@ -6,9 +6,11 @@ import { Component, OnInit, Input } from '@angular/core';
       <h3>Test Component</h3>
       <!--<h3>{{"Hello "+parentData}}</h3>-->
       <h3>{{"Hello "+name}}</h3>
+
+      <button (click) = "fireEvent()">Send Event</button>
   `,
   styles: [`
-      h2, h3 {text-align: center;}
+      h2, h3, button {text-align: center;}
   `]
 })
 export class TestComponent implements OnInit {
@@ -16,6 +18,11 @@ export class TestComponent implements OnInit {
   //@Input() public parentData;
   @Input('parentData') public name;
 
+  @Output() public childEvent = new EventEmitter();
+
+  fireEvent(){
+    this.childEvent.emit("Hey Angular");
+  }
   constructor() { }
 
   ngOnInit(): void {
