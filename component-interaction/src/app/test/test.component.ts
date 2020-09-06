055573c91ed266adc93006f347e37fb003bfccb5
Dipak Aher <dipakaher@Dipaks-MacBook-Air.local>
2020-09-06 13:17:23 +0530

    Sending data from parentComponent to ChildComponent

--- component-interaction/src/app/test/test.component.ts
+++ component-interaction/src/app/test/test.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-test',
   template: `
       <h3>Test Component</h3>
+      <!--<h3>{{"Hello "+parentData}}</h3>-->
+      <h3>{{"Hello "+name}}</h3>
   `,
   styles: [`
-      h3 {text-align: center;}
+      h2, h3 {text-align: center;}
   `]
 })
 export class TestComponent implements OnInit {
+
+  //@Input() public parentData;
+  @Input('parentData') public name;
 
   constructor() { }
 
