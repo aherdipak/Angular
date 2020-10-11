76864f93c8fbc75933b4d962a6ff7af37ed50915
aherdipak3@gmail.com <aherdipak3@gmail.com>
2020-10-11 15:28:26 +0530

    Routing in angular

---
+++ routing-demo/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { DepartmentListComponent } from './department-list/department-list.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+
+// If we navigate department then we need to display DepartmentListComponent
+const routes: Routes = [
+  {path: 'department', component: DepartmentListComponent}, 
+  {path: 'employee', component: EmployeeListComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+export const routingComponents = [DepartmentListComponent, EmployeeListComponent]
